0e772bd1e8657d8650100c871c1c9b730db5066e
inhere <in.798@qq.com>
2019-01-24 01:01:20 +0800

    add some new test

--- test/ScopedValidatorsTest.php
+++ test/ScopedValidatorsTest.php
@@ -8,11 +8,39 @@
 
 namespace Inhere\ValidateTest;
 
+use Inhere\Validate\Validation;
+use PHPUnit\Framework\TestCase;
+
 /**
  * Class ScopedValidatorsTest
  * @package Inhere\ValidateTest
  */
-class ScopedValidatorsTest
+class ScopedValidatorsTest extends TestCase
 {
+    /**
+     * lt, lte, ...
+     */
+    public function testFiledCompare()
+    {
+        $v = Validation::make([
+            'name' => 'tom',
+            'age' => 34,
+            'tags' => ['funny', 'smile'],
+        ]);
 
+        // lt
+        $this->assertTrue($v->ltFieldValidator(23, 'age'));
+        $this->assertFalse($v->ltFieldValidator('23', 'age'));
+        $this->assertFalse($v->ltFieldValidator(34, 'age'));
+
+        // lte
+        $this->assertTrue($v->lteFieldValidator(23, 'age'));
+        $this->assertTrue($v->lteFieldValidator(34, 'age'));
+        $this->assertFalse($v->lteFieldValidator('34', 'age'));
+
+        // gte
+        $this->assertTrue($v->gteFieldValidator(43, 'age'));
+        $this->assertTrue($v->gteFieldValidator(34, 'age'));
+        $this->assertFalse($v->gteFieldValidator('34', 'age'));
+    }
 }
